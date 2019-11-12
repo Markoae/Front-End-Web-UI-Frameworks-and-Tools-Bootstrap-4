c43d36f5377aa38a16519cfc5a4b5285d52a8955
Marco Aurelio <aurelio.estevam@gmail.com>
2019-11-12 03:13:01 -0300

    Gulp Part 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-Sync');
+    browserSync = require('browser-Sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
  
 gulp.task('sass', function () {
@@ -33,3 +41,39 @@ gulp.task('browser-sync', function () {
       
 // Default task
 gulp.task('default', gulp.series('browser-sync', 'sass:watch'));
+
+// Clean
+gulp.task('clean', function() {
+   return del(['dist']);
+});
+
+gulp.task('copyfonts', function() {
+   return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+   return gulp.src('img/*.{png,jpg,gif}')
+     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+     .pipe(gulp.dest('dist/img'));
+ });
+
+ gulp.task('usemin', function() {
+   return gulp.src('./*.html')
+   .pipe(flatmap(function(stream, file){
+       return stream
+         .pipe(usemin({
+             css: [ rev() ],
+             html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+             js: [ uglify(), rev() ],
+             inlinejs: [ uglify() ],
+             inlinecss: [ cleanCss(), 'concat' ]
+         }))
+     }))
+     .pipe(gulp.dest('dist/'));
+ });
+
+ gulp.task('build', gulp.series('clean', 'copyfonts', 'imagemin', 'usemin', function (done) { done(); }
+
+ ));
